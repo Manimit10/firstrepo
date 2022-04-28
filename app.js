e788084b9370aa4cc9ede534af9d54d5c3060ed0
mani <m.tahriri.z@gmail.com>
2022-04-28 11:03:54 +0200

    I am so happy today

---
+++ app.js
@@ -0,0 +1,17 @@
+let text = document.getElementById('input')
+let output = document.getElementById('output')
+output.innerText = localStorage.getItem('myshake')
+
+text.addEventListener('keyup', func)
+function func(){
+    output.innerText = text.value
+    localStorage.setItem('myshake', text.value)
+}
+
+let btn = document.getElementById('btn')
+let paragraph = document.querySelector('.vis')
+btn.addEventListener('click', ()=>{
+console.log(paragraph)
+paragraph.style.color = 'blue'
+
+})
